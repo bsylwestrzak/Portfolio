a9a32e40526d02b5f588afa91efa632b2d920189
Mikołaj Wiśniewski <m.wisniewski@pirxon.com>
2020-06-16 17:08:08 +0200

    Form implementation, validation applied

--- mail.php
+++ mail.php
@@ -1,3 +1,7 @@
+<?php
+  require_once('webmail/helpers.php');
+?>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -42,18 +46,24 @@
     
 
     <div class="container">
-        <form>
+        <form action="webmail/index.php" method="POST">
+
+          <?php mailSucceed('<div class="success">Dziękujemy za kontakt. Postaramy się niezwłocznie odpowiedzieć na wysłaną wiadomość.</div>'); ?>
+          <?php hasError('firstname', '<div class="error">&#9888; Podane nazwisko musi mieć od 3 do 40 znaków!</div>'); ?>
           <label for="fname">First Name</label>
-          <input type="text" id="fname" name="firstname" placeholder="Your name..">
+          <input value="<?php previousVal('firstname'); ?>" type="text" id="fname" name="firstname" placeholder="Your name..">
       
+          <?php hasError('lastname', '<div class="error">&#9888; Podane nazwisko musi mieć od 3 do 40 znaków!</div>'); ?>
           <label for="lname">Last Name</label>
-          <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+          <input value="<?php previousVal('lastname'); ?>" type="text" id="lname" name="lastname" placeholder="Your last name..">
       
+          <?php hasError('email', '<div class="error">&#9888; Podany email jest wadliwy!</div>'); ?>
           <label for="lname">Your email</label>
-          <input type="text" id="email" name="email" placeholder="Your email..">
+          <input value="<?php previousVal('email'); ?>" type="text" id="email" name="email" placeholder="Your email..">
       
+          <?php hasError('content', '<div class="error">&#9888; Wiadomość musi się mieścić od 20 do 500 znaków!</div>'); ?>
           <label for="subject">Subject</label>
-          <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+          <textarea id="subject" name="content" placeholder="Write something.." style="height:200px"><?php previousVal('content'); ?></textarea>
       
           <input type="submit" value="Submit">
         </form>
